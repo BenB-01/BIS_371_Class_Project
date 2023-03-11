97fbdaa567323a168e281f4cbded2c1567f606ea
BenB-01 <benburkert3@gmail.com>
2023-03-10 20:18:47 -0800

    script for inserting the values for the nontransaction tables

---
+++ Source/database/values_nontransaction_tables.sql
@@ -0,0 +1,64 @@
+INSERT INTO Department (Department_Name)
+VALUES ('Accounting'), ('Marketing'), ('Finance'), ('BIS'), ('Management'), ('Entrepreneurship');
+
+INSERT INTO Activity_Type (Activity_Type_Name)
+VALUES ('submitted'), ('accepted'), ('r&r'), ('rejected');
+
+INSERT INTO Role (Role_Type_Name)
+VALUES ('Contributor'), ('Lead'), ('Co_lead');
+
+INSERT INTO Target_Type (Target_Type_Name)
+VALUES ('journal'), ('conference');
+
+INSERT INTO People (F_Name, L_Name)
+VALUES
+('Josie', 'Ross'),
+('Tallulah', 'Stewart'),
+('Bridget', 'Cox'),
+('Sally', 'Miller'),
+('Bella', 'Foster'),
+('Sapphire', 'Carter'),
+('Amelia', 'Lewis'),
+('Mindy', 'Garcia'),
+('Ebony', 'Taylor'),
+('Carrie', 'Perry'),
+('Anaya', 'Kelly'),
+('Ayat', 'Reed'),
+('Ria', 'Green'),
+('Klaudia', 'Jenkins'),
+('Myah', 'Watson'),
+('Alma', 'Hughes'),
+('Hannah', 'Turner'),
+('Henna', 'Baker'),
+('May', 'Jackson'),
+('Jo', 'Barnes'),
+('Tayla', 'Sanchez'),
+('Bailey', 'Howard'),
+('Louis', 'Bennett'),
+('Caitlan', 'Butler'),
+('Darcey', 'Davis'),
+('Alfred', 'White'),
+('Shayne', 'Thomas'),
+('Melvin', 'Richardson'),
+('Marvin', 'Cook'),
+('Leyton', 'Ward'),
+('Jesse', 'Wright'),
+('Barney', 'Torres'),
+('Casper', 'Brown'),
+('Aarav', 'Roberts'),
+('Alexander', 'Morgan'),
+('Wesley', 'Adams'),
+('Carwyn', 'Rogers'),
+('Simone', 'Clark'),
+('Wilbur', 'Parker'),
+('Kinsley', 'Thompson'),
+('Zakaria', 'Cooper'),
+('Garfield', 'Anderson'),
+('Pierce', 'Bailey'),
+('Dan', 'Allen'),
+('Rick', 'Wilson'),
+('Colin', 'Jones'),
+('Ayub', 'Myers'),
+('Robert', 'Brooks'),
+('Darcy', 'Sullivan'),
+('Howard', 'Jones');
